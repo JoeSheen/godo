b75e78fa057c045f770e851f840fd6efda64e463
Joe Sheen <joe123sheen@hotmail.com>
2024-09-27 21:29:47 +0100

    updated tasks.sql migration file

--- migrations/20240927202812_tasks.sql
+++ migrations/20240927202812_tasks.sql
@@ -1,13 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS tasks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER NOT NULL,
     title VARCHAR(255) NOT NULL,
     priority INTEGER CHECK (priority IN (0, 5)),
     completed BOOLEAN NOT NULL CHECK (completed IN (0, 1)),
+    category VARCHAR(255),
     created_timestamp datetime not null,
     completed_timestamp datetime
-    ceadline datetime
+    deadline datetime,
+    PRIMARY KEY(id)
 );
 -- +goose StatementEnd
 
